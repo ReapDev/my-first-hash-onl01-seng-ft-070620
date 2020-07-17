7680f4102a203634be97b2f8cb5f8038978e0f85
Brandon Williams <nis350ztt@gmail.com>
2020-07-17 19:15:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = { "my_stuff" => "not your stuff", "my_age" => 31 }
   
 end
 
